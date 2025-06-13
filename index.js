5beb250bb984972a8958421e76320ef53b3562ae
Alina Bartashchuk <alina@MacMini008.local>
2025-06-13 11:32:18 +0200

    Lebenslauf

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi")
+window.addEventListener("DOMContentLoaded", function(){
+    const menuButton = document.getElementById("menuButton");
+    const menu = document.getElementById("mainMenu");
+
+    menuButton.addEventListener("click", 
+        function() {
+            if (menu.style.display === "none" || menu.style.display === "") {
+                menu.style.display = "block";
+            } else{
+                menu.style.display = "none"
+            }
+        });
+});
